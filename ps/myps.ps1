8fcddb718e8f9793a21bd1b4f6bb25f3bb4eaca5
Kiran Reddy <v.2kirar@gmail.com>
2022-11-05 22:45:46 +0530

    ps

---
+++ ps/myps.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $first,
+
+    [Parameter()]
+    [string]
+    $second
+)
+
+write-verbose ("this is firstenv:{0}" -f ${env.firstenv})
+
+${env.firstenv} | ConvertTo-Json
+$env:firstenv | ConvertTo-Json
+
+$first = $env:firstenv
+
+switch($first)
+{
+    'my first env' {Write-Verbose "yup that worked";break}
+    default {Write-Warning 'that didnt work mate'}
+}
