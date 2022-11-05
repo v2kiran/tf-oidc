8786a68f89e8fd48173aa0815357c983f09f5cdd
Kiran Reddy <v.2kirar@gmail.com>
2022-11-05 23:19:05 +0530

    various

--- ps/myps.ps1
+++ ps/myps.ps1
@@ -9,14 +9,21 @@ param (
     $second
 )
 
-write-verbose ("this is firstenv:{0}" -f ${env.firstenv})
+write-verbose ("this is env:firstenv:{0}" -f $env:firstenv)
 
-${env.firstenv} | ConvertTo-Json
+$env:firstenv
+Write-Verbose "after printing firstenv"
+($env:firstenv).tostring()
 $env:firstenv | ConvertTo-Json
 
 $first = $env:firstenv
 
-switch($first)
+Write-Verbose ("this is first : [{0}]" -f $first)
+Write-Verbose ("this is first to string: [{0}]" -f $first.ToString())
+
+
+
+switch ($first)
 {
     'my first env' {Write-Verbose "yup that worked";break}
     default {Write-Warning 'that didnt work mate'}
