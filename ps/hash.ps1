aedd0e29466b4af5121c81f9b1b4e36c7cfc845a
Kiran Reddy <v.2kirar@gmail.com>
2022-11-06 00:52:15 +0530

    hash

---
+++ ps/hash.ps1
@@ -0,0 +1,16 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $first,
+
+    [Parameter()]
+    [string]
+    $second
+)
+
+
+@{
+    NUMBER3 = "NUMBER3"
+    NUMBER4 = "NUMBER4"
+}
