9f22dd147ccd276b886f62b40fd23ecfa72d1ad3
Kiran Reddy <v.2kirar@gmail.com>
2022-11-06 00:42:55 +0530

    outputs

---
+++ ps/output.ps1
@@ -0,0 +1,16 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $first,
+
+    [Parameter()]
+    [string]
+    $second
+)
+
+
+[PSCustomObject]@{
+    NUMBER1 = "NUMBER-1"
+    NUMBER2 = "NUMBER-2"
+}
