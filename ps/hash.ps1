3c5785e5787e4d7e2321ecd9feec6e325c177aea
Kiran Reddy <v.2kirar@gmail.com>
2022-11-09 00:03:32 +0530

    test

--- ps/hash.ps1
+++ ps/hash.ps1
@@ -10,7 +10,13 @@ param (
 )
 
 
-[PSCustomObject]@{
+$myout = [PSCustomObject]@{
     NUMBER3 = 'NUMBER-3'
     NUMBER4 = 'NUMBER-4'
 }
+
+$myout
+
+$myout | Get-Member -MemberType NoteProperty | ForEach-Object {
+    "$($_.Name)=$($myout.$($_.Name))"
+} | Out-File -FilePath $Env:GITHUB_ENV -Append
