71c90265b2f82fcd31184cd69d1647af2dca2f7e
Kiran Reddy <v.2kirar@gmail.com>
2022-11-10 22:57:37 +0530

    here

---
+++ ps/mark.ps1
@@ -0,0 +1,11 @@
+﻿param($header1,$header2)
+
+
+$here = @"
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+"@
+
+$here >> $GITHUB_STEP_SUMMARY
