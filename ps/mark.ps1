070212b7d0dcdbc0e7fa82b89c54901d6ed9c3ea
Kiran Reddy <v.2kirar@gmail.com>
2022-11-10 23:14:20 +0530

    service table

--- ps/mark.ps1
+++ ps/mark.ps1
@@ -1,12 +1,23 @@
 ﻿param($header1,$header2)
 
-
+$ser = gsv wl* | select Name,DisplayName,Status
 $here = @"
 | $header1  | $header2 |
 | ------------- | ------------- |
 | Content Cell  | Content Cell  |
 | Content Cell  | Content Cell  |
+
+
+
+| Name  | DisplayName | Status |
+$(foreach ($s in $ser)
+{
+`| $s.name `| $s.DisplayName `| $s.Status `|
+})
 "@
 $here
+
+
+
 
 $here | out-file  $env:GITHUB_STEP_SUMMARY -Append
