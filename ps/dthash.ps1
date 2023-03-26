d64bfcf98b78b4aad7a1ad2c708fe22dbd0ef8c0
Kiran Reddy <v.2kirar@gmail.com>
2023-03-26 21:31:06 +0530

    testing dt hash

---
+++ ps/dthash.ps1
@@ -0,0 +1,10 @@
+﻿
+$hash = $env:Datatopologies
+$hash.GetEnumerator() | ForEach-Object {
+    $seq = $_.Key
+    $nested_hash = $_.value
+
+    Write-Verbose "this is the sub: $($nested_hash['sub'])" -Verbose
+    Write-Verbose "this is the name: $($nested_hash['name'])" -Verbose
+
+}
