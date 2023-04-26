ad0373ec7f1ffec4ce482dc42ebacbc7abfea2d8
Kiran Reddy <v.2kirar@gmail.com>
2023-04-26 22:25:34 +0530

    eof

--- ps/load-secret.ps1
+++ ps/load-secret.ps1
@@ -1,7 +1,6 @@
 ﻿
-$cert_path = "ps/certs/test.pem"
-$cert_content =  @"
-$(gc $cert_path -Raw)
-"@
+$cert_path = 'ps/certs/test.pem'
 
-"cert=$cert_content" | Out-File $env:GITHUB_ENV
+Write-Output 'cert<<EOF' | Out-File $env:GITHUB_ENV
+Get-Content $cert_path -Raw  | Out-File $env:GITHUB_ENV
+Write-Output 'EOF' | Out-File $env:GITHUB_ENV
